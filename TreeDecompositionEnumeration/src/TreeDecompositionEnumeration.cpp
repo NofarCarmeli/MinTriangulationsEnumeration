abb74332891f7bb5d530b70cbb4a54cfd6523847
NofarCarmeli <carmelinofar@gmail.com>
2016-05-23 11:15:41 +0300

    Added time count

--- TreeDecompositionEnumeration/src/TreeDecompositionEnumeration.cpp
+++ TreeDecompositionEnumeration/src/TreeDecompositionEnumeration.cpp
@@ -1,26 +1,40 @@
 #include <iostream>
-#include "Graph.h"
-#include "MinimalSeparatorsEnumerator.h"
-#include "SeparatorGraph.h"
-#include "MinimalTriangulationsEnumerator.h"
-#include "IndependentSetExtender.h"
-#include "converter.h"
+#include <ctime>
 #include "GraphReader.h"
-#include "IndSetExtByTriangulation.h"
+#include "MinimalTriangulationsEnumerator.h"
 using namespace std;
 using namespace tdenum;
 
+void printTimeElapsed(double timeElapsed) {
+	cout.precision(2);
+	cout << "Computation took " << fixed << (1000.0 * timeElapsed)/CLOCKS_PER_SEC
+			<< " milliseconds" << endl;
+}
+
 int main() {
 	Graph g = GraphReader::read();
+	clock_t startTime, endTime;
 	MinimalTriangulationsEnumerator enumerator(g);
 
 	int resultNumber = 1;
+	double totalTime = 0;
+
+	startTime = clock();
 	while (enumerator.hasNext()) {
+		Graph triangulation = enumerator.next();
+		endTime = clock();
+		double lastTime = difftime(endTime, startTime);
+		totalTime += lastTime;
+
 		cout << "Triangulation #" << resultNumber << ":" << endl;
-		enumerator.next().print();
-		resultNumber++;
+		triangulation.print();
+		printTimeElapsed(lastTime);
 		cout << endl;
+
+		resultNumber++;
+		startTime = clock();
 	}
 	cout << "All minimal triangulations were printed!" << endl;
+	printTimeElapsed(totalTime);
 	return 0;
 }
