6969e97a17eaaf9e9bd104230adff362909052e5
NofarCarmeli <carmelinofar@gmail.com>
2016-06-02 15:16:43 +0300

    Change output format and add time limit

--- TreeDecompositionEnumeration/src/TreeDecompositionEnumeration.cpp
+++ TreeDecompositionEnumeration/src/TreeDecompositionEnumeration.cpp
@@ -5,11 +5,37 @@
 using namespace std;
 using namespace tdenum;
 
-void printTimeElapsed(double timeElapsed) {
-	cout.precision(2);
-	cout << "Computation took " << fixed << (1000.0 * timeElapsed)/CLOCKS_PER_SEC
-			<< " milliseconds" << endl;
+double clocksToMillis(double timeElapsed) {
+	return (1000.0 * timeElapsed)/CLOCKS_PER_SEC;
 }
+
+double millisToclocks(double time) {
+	return (time * CLOCKS_PER_SEC) / 1000.0;
+}
+
+class Result {
+	int number;
+	double time;
+	int fill;
+	int treewidth;
+	Graph origin;
+	ChordalGraph triangulation;
+public:
+	Result() {}
+	Result(int index, double t, Graph g, ChordalGraph triangulation) :
+			number(index), origin(g), triangulation(triangulation) {
+		time = clocksToMillis(t);
+		fill = triangulation.getFillIn(g);
+		treewidth = triangulation.getTreeWidth();
+	}
+	int getFill() { return fill; }
+	int getWidth() { return treewidth; }
+	void print() {
+		cout << number << ") Time " << time << " millis, Tree-width "
+				<< treewidth << ", Fill-in " << fill << endl;
+		//triangulation.printTriangulation(origin); cout << endl;
+	}
+};
 
 int main() {
 	Graph g = GraphReader::read();
@@ -18,25 +44,57 @@ int main() {
 
 	int resultNumber = 1;
 	double totalTime = 0;
+	vector<Result> results;
+	Result minWidth;
+	Result minFill;
+
+	double timeLimitInMillis = 10000;
+	bool timeLimitExceeded = false;
 
 	startTime = clock();
 	while (enumerator.hasNext()) {
 		ChordalGraph triangulation = enumerator.next();
+
 		endTime = clock();
 		double lastTime = difftime(endTime, startTime);
 		totalTime += lastTime;
 
-		cout << "Triangulation #" << resultNumber << ":" << endl;
-		triangulation.printTriangulation(g);
-		cout << triangulation.getFillIn(g) << " fill edges" << endl;
-		cout << "Tree-width is " << triangulation.getTreeWidth() << endl;
-		printTimeElapsed(lastTime);
-		cout << endl;
+		Result res(resultNumber, totalTime, g, triangulation);
+		results.push_back(res);
+		res.print();
+
+		if (resultNumber == 1) {
+			minWidth = res;
+			minFill = res;
+		} else {
+			if (res.getWidth() < minWidth.getWidth()) {
+				minWidth = res;
+			}
+			if (res.getFill() < minFill.getFill()) {
+				minFill = res;
+			}
+		}
+
+		if (clocksToMillis(totalTime) >= timeLimitInMillis) {
+			timeLimitExceeded = true;
+			break;
+		}
 
 		resultNumber++;
 		startTime = clock();
 	}
-	cout << "All minimal triangulations were printed!" << endl;
-	printTimeElapsed(totalTime);
+
+	if (timeLimitExceeded) {
+		cout << "Time limit reached" << endl << endl;
+	} else {
+		cout << "All minimal triangulations were printed!" << endl << endl;
+	}
+	cout << "First result: ";
+	results[0].print();
+	cout << "Lowest fill: ";
+	minFill.print();
+	cout << "Lowest tree-width: ";
+	minWidth.print();
+	cout << "Total time " << clocksToMillis(totalTime) << " milliseconds" << endl;
 	return 0;
 }
