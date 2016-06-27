8d0a26ec138e9f38197df97d2ffd721501bdaf27
NofarCarmeli <carmelinofar@gmail.com>
2016-06-27 16:55:13 +0300

    Add scoring files

---
+++ TreeDecompositionEnumeration/src/IndSetScorerByTriangulation.cpp
@@ -0,0 +1,43 @@
+#include "IndSetScorerByTriangulation.h"
+#include "ChordalGraph.h"
+#include "Converter.h"
+
+namespace tdenum {
+
+IndSetScorerByTriangulation::IndSetScorerByTriangulation(const Graph& g, TriangulationScoringCriterion c) :
+		graph(g), criterion(c) {}
+
+IndSetScorerByTriangulation::~IndSetScorerByTriangulation() {}
+
+int IndSetScorerByTriangulation::scoreIndependentSet(const set<MinimalSeparator>& s) {
+	if (criterion == NONE) {
+		return 0;
+	} else if (criterion == WIDTH) {
+		ChordalGraph g = Converter::minimalSeparatorsToTriangulation(graph, s);
+		return g.getTreeWidth();
+	} else if (criterion == FILL) {
+		ChordalGraph g = Converter::minimalSeparatorsToTriangulation(graph, s);
+		return g.getFillIn(graph);
+	} else if (criterion == MAX_SEP_SIZE) {
+		int maxSeparatorSize = 0;
+		for (set<MinimalSeparator>::iterator it = s.begin(); it!=s.end(); ++it) {
+			if (it->size() > (unsigned int)maxSeparatorSize) {
+				maxSeparatorSize = it->size();
+			}
+		}
+		return maxSeparatorSize;
+	} else if (criterion == DIFFERENECE) {
+		int score = 0;
+		ChordalGraph g = Converter::minimalSeparatorsToTriangulation(graph, s);
+		set< set<Node> > fillEdges = g.getFillEdges(graph);
+		for (set< set<Node> >::iterator it=fillEdges.begin(); it!=fillEdges.end(); ++it) {
+			if (seenFillEdges.count(*it) > 0) {
+				score++;
+			}
+		}
+		return score;
+	}
+	return 0;
+}
+
+} /* namespace tdenum */
