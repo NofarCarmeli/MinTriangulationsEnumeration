47a9c45956a401c28b36075992f9c8ebc65fef02
NofarCarmeli <carmelinofar@gmail.com>
2024-03-18 19:21:00 +0100

    Separate ResultsHandler file

--- makefile
+++ makefile
@@ -1,7 +1,7 @@
 ODIR=obj
 SDIR=src
-_DEPS = IndependentSetExtender.h IndependentSetScorer.h MaximalIndependentSetsEnumerator.h ResultsHandler.h IndSetExtByTriangulation.h IndSetExtBySeparators.h IndSetScorerByTriangulation.h MinimalSeparatorsEnumerator.h MinimalTriangulationsEnumerator.h GraphReader.h MinimalTriangulator.h Graph.h DataStructures.h SeparatorGraph.h Converter.h SeparatorsScorer.h ChordalGraph.h SubGraph.h SuccinctGraphRepresentation.h
-_OBJ = MinTriangulationsEnumeration.o IndSetExtByTriangulation.o IndSetExtBySeparators.o IndSetScorerByTriangulation.o MinimalSeparatorsEnumerator.o MinimalTriangulationsEnumerator.o GraphReader.o MinimalTriangulator.o Graph.o DataStructures.o SeparatorGraph.o Converter.o SeparatorsScorer.o ChordalGraph.o SubGraph.o
+_DEPS = IndSetExtByTriangulation.h IndSetExtBySeparators.h IndSetScorerByTriangulation.h MinimalSeparatorsEnumerator.h MinimalTriangulationsEnumerator.h GraphReader.h MinimalTriangulator.h Graph.h DataStructures.h SeparatorGraph.h Converter.h SeparatorsScorer.h ChordalGraph.h SubGraph.h ResultsHandler.h SuccinctGraphRepresentation.h IndependentSetExtender.h IndependentSetScorer.h MaximalIndependentSetsEnumerator.h
+_OBJ = IndSetExtByTriangulation.o IndSetExtBySeparators.o IndSetScorerByTriangulation.o MinimalSeparatorsEnumerator.o MinimalTriangulationsEnumerator.o GraphReader.o MinimalTriangulator.o Graph.o DataStructures.o SeparatorGraph.o Converter.o SeparatorsScorer.o ChordalGraph.o SubGraph.o ResultsHandler.o MinTriangulationsEnumeration.o 
  
 CC=g++
 CFLAGS=-I$(SDIR)
--- src/ResultsHandler.h
+++ src/ResultsHandler.h
@@ -7,6 +7,7 @@
 
 #include <ostream>
 #include <ctime>
+#include "ChordalGraph.h"
 using namespace std;
 
 namespace tdenum {
@@ -29,10 +30,7 @@ public:
 	int getFill() { return fill; }
 	int getWidth() { return width; }
 	long long getExpBagSize() { return expBagSize; }
-	void printSummary(ostream& output) {
-		output << "#" << number << " (width " << width << ", fill " << fill
-				<< ", sum of exponents of bag sizes " << expBagSize << "), obtained after " << time << " seconds." << endl;
-	}
+	void printSummary(ostream& output);
 };
 
 enum OutputForm { TRIANGULATIONS, BAGSETS };
@@ -59,82 +57,8 @@ public:
 				maxBagExpSize(0), resultsFound(0) {
 		startTime = clock();
 	}
-	void newResult(const ChordalGraph& triangulation) {
-		ResultInformation currentResult(++resultsFound, getTime(), inputGraph, triangulation);
-		int width = currentResult.getWidth();
-		int fill = currentResult.getFill();
-		long long bagExpSize = currentResult.getExpBagSize();
-		
-		// store metrics ranges
-		if (resultsFound == 1) {
-			minBagExpSizeResult = minFillResult = minWidthResult = currentResult;
-			minWidth = maxWidth = width;
-			minFill = maxFill = fill;
-			minBagExpSize = maxBagExpSize = bagExpSize;
-		} else {
-			if (width < minWidth) {
-				minWidthResult = currentResult;
-				minWidth = width;
-			} else if (width > maxWidth) {
-				maxWidth = width;
-			}
-			if (fill < minFill) {
-				minFillResult = currentResult;
-				minFill = fill;
-			} else if (fill > maxFill) {
-				maxFill = fill;
-			}
-			if (bagExpSize < minBagExpSize) {
-				minBagExpSizeResult = currentResult;
-				minBagExpSize = bagExpSize;
-			} else if (bagExpSize > maxBagExpSize) {
-				maxBagExpSize = bagExpSize;
-			}
-		}
-
-		// Print triangulation
-		currentResult.printSummary(output);
-		if (outputForm == TRIANGULATIONS) {
-			triangulation.printTriangulation(output, inputGraph);
-		} else if (outputForm == BAGSETS) {
-			triangulation.printMaximalCliques(output);
-		}
-		output << " " << endl;
-	}
-	
-	void printReadableSummary(ostream& output) {
-		// Print count and metrics ranges
-		output << resultsFound << " triangulation were found in " << getTime() << " seconds (";
-		if (minWidth == maxWidth) {
-			output << "Width " << minWidth << ", ";
-		} else {
-			output << "Width " << minWidth << "-" << maxWidth << ", ";
-		}
-		if (minFill == maxFill) {
-			output << "fill " << minFill << ", ";
-		} else {
-			output << "fill " << minFill << "-" << maxFill << ", ";
-		}
-		if (minBagExpSize == maxBagExpSize) {
-			output << "sum of exponents of bag sizes " << minBagExpSize << ".";
-		} else {
-			output << "sum of exponents of bag sizes " << minBagExpSize
-					<< "-" << maxBagExpSize << ").";
-		}
-		output <<endl;
-
-		// Recommend triangulations
-		if (resultsFound > 1) {
-			output << "Recommended results:" << endl;
-			minBagExpSizeResult.printSummary(output);
-			if (minFill < minBagExpSizeResult.getFill()) {
-				minFillResult.printSummary(output);
-			}
-			if (minWidth < minBagExpSizeResult.getWidth()) {
-				minWidthResult.printSummary(output);
-			}
-		}
-	}
+	void newResult(const ChordalGraph& triangulation);
+	void printReadableSummary(ostream& output);
 };
 
 
